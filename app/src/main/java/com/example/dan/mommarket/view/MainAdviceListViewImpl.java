3cd17000a66130ec9bad7149a563118cd8ba23d4
Danil Yaschenko <danyaschenko@gmail.com>
2016-08-31 18:14:05 +0300

    add product_card to advice detail

--- app/src/main/java/com/example/dan/mommarket/view/MainAdviceListViewImpl.java
+++ app/src/main/java/com/example/dan/mommarket/view/MainAdviceListViewImpl.java
@@ -5,12 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.dan.mommarket.Navigator;
 import com.example.dan.mommarket.R;
-import com.example.dan.mommarket.adapter.AdviceListRVAdapter;
+import com.example.dan.mommarket.adapter.MainAdviceListRVAdapter;
 import com.example.dan.mommarket.model.Advice;
 import com.example.dan.mommarket.presenter.MainAdviceListPresenter;
 import com.example.dan.mommarket.presenter.MainAdviceListPresenterImpl;
@@ -21,26 +23,38 @@ import java.util.List;
  * Created by dan on 26.08.16.
  */
 
-public class MainAdviceListViewImpl extends Fragment implements MainAdviceListView, AdviceListRVAdapter.OnAdviceClickListener {
+public class MainAdviceListViewImpl extends Fragment implements MainAdviceListView, MainAdviceListRVAdapter.OnAdviceClickListener {
 
 
     MainAdviceListPresenter mainAdviceListPresenter;
     private View v;
-    private AdviceListRVAdapter adviceListRVAdapter;
+    private MainAdviceListRVAdapter mainAdviceListRVAdapter;
     private RecyclerView recyclerView;
-
     private Navigator navigator;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.fragment_advice_list, container, false);
+        v = inflater.inflate(R.layout.fragment_main_advice_list, container, false);
         recyclerView = (RecyclerView) v.findViewById(R.id.rv_advice_list);
 //        catalogPresenter = new CatalogPresenterImpl(context);
         mainAdviceListPresenter = MainAdviceListPresenterImpl.getInstance();
         mainAdviceListPresenter.setView(this);
         mainAdviceListPresenter.onCreateView(savedInstanceState);
         return v;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(getActivity(), "HELL YEAH BITCHES " + item.getItemId(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(getActivity(), "HELL YEAH BITCHES", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -51,8 +65,8 @@ public class MainAdviceListViewImpl extends Fragment implements MainAdviceListVi
 
     @Override
     public void showAdvices(List<Advice> adviceList) {
-        adviceListRVAdapter = new AdviceListRVAdapter(adviceList, getContext(), this);
-        recyclerView.setAdapter(adviceListRVAdapter);
+        mainAdviceListRVAdapter = new MainAdviceListRVAdapter(adviceList, getContext(), this);
+        recyclerView.setAdapter(mainAdviceListRVAdapter);
     }
 
     @Override
