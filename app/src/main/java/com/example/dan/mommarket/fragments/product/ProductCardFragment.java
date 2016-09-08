311805b8d81fa866c372822f97c856370ee158c3
Danil Yaschenko <danyaschenko@gmail.com>
2016-09-08 15:27:38 +0300

    add recycler view to product_card + design for product_card

--- app/src/main/java/com/example/dan/mommarket/fragments/product/ProductCardFragment.java
+++ app/src/main/java/com/example/dan/mommarket/fragments/product/ProductCardFragment.java
@@ -3,18 +3,31 @@ package com.example.dan.mommarket.fragments.product;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dan.mommarket.R;
+import com.example.dan.mommarket.adapter.ShopListAdapter;
+import com.example.dan.mommarket.model.Shop;
 import com.example.dan.mommarket.views.ProductCard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dan on 06.09.16.
  */
 
 public class ProductCardFragment extends Fragment implements ProductCard {
+    private RecyclerView recyclerView;
+    private ShopListAdapter shopListAdapter;
+
+    private List<Shop> shopList;
+    Shop shop;
+
+
     @Override
     public void showProduct() {
 
@@ -23,7 +36,18 @@ public class ProductCardFragment extends Fragment implements ProductCard {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_products_card, container, false);
+        View v = inflater.inflate(R.layout.fragment_product_card, container, false);
+        recyclerView = (RecyclerView) v.findViewById(R.id.rv_product_card_shops);
+        shopList = new ArrayList<>();
+        shop = new Shop();
+        shop.setName("Lara Kids");
+        shopList.add(shop);
+        shopList.add(shop);
+        shopList.add(shop);
+        shopList.add(shop);
+        shopListAdapter = new ShopListAdapter(shopList);
+        recyclerView.setAdapter(shopListAdapter);
+        recyclerView.setNestedScrollingEnabled(false);
         return v;
     }
 }
