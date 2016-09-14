7a1d1b8d87e661a04b562f70456d40b850731ab1
Danil Yaschenko <danyaschenko@gmail.com>
2016-09-14 19:11:51 +0300

    fix navigator

--- app/src/main/java/com/example/dan/mommarket/MainActivity.java
+++ app/src/main/java/com/example/dan/mommarket/MainActivity.java
@@ -233,6 +233,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void navigateToCatalog() {
+        clearBackStack();
         getSupportActionBar().setTitle("Каталог");
         CategoryRootFragment categoryRootFragment = new CategoryRootFragment();
         getSupportFragmentManager().
